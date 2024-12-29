ce19ee631e779c73245993bd99916eebd1ece801
crossing-c <57180755+crossing-c@users.noreply.github.com>
2024-12-29 11:41:57 +0800

    Add files via upload

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace DQMS
+{
+    public class BundleConfig
+    {
+        // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/assets/js/jquery.min.js",
+                        "~/assets/js/jquery.cookie.js",
+                        "~/assets/js/bootstrap.min.js",
+                        "~/assets/js/jquery.mCustomScrollbar.concat.min.js",
+                        "~/assets/js/custom.js",
+                        "~/assets/js/jquery.token.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/assets/css/bootstrap.min.css",
+                      "~/assets/css/font-awesome.min.css",
+                      "~/assets/css/jquery.mCustomScrollbar.min.css",
+                      "~/assets/css/custom-white.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/black-css").Include(
+                     "~/assets/css/bootstrap.min.css",
+                     "~/assets/css/font-awesome.min.css",
+                     "~/assets/css/jquery.mCustomScrollbar.min.css",
+                     "~/assets/css/custom-black.min.css"));
+        }
+    }
+}
---
+++ App_Start/MyViewEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace DQMS
+{
+    public sealed class MyViewEngine : RazorViewEngine
+    {
+
+        public MyViewEngine()
+        {
+            ViewLocationFormats = new[]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml"
+                //"~/Views/Shared/BaseCharts/{0}.cshtml"//我们的规则
+            };
+
+            PartialViewLocationFormats = new[]
+            {
+                "~/Views/{1}/{0}.cshtml",
+                "~/Views/Shared/{0}.cshtml"
+               // "~/Views/Shared/BaseCharts/{0}.cshtml"//我们的规则
+            };
+        }
+        public override ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            return base.FindView(controllerContext, viewName, masterName, useCache);
+        }
+        public override ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            return base.FindPartialView(controllerContext, partialViewName, useCache);
+        }
+    }
+}
